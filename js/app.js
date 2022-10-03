8779a0f8d4cbe0d97385d0f683a5fff192f87e43
sherzoDev <mustafoqulovsherzodbek@gmail.com>
2022-10-03 15:34:20 +0500

    Intial commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 var elBtn = document.querySelector(".header__btn");
+var body = document.querySelector("body");
 
 elBtn.addEventListener("click", function() {
   elBtn.parentElement.classList.toggle("show-bg");
+  body.classList.toggle("scroll");
 })
